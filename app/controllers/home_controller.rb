9b143f5bdf4d7b12b90c91cf508fe7d6b28d494e
jorgemtoledo <jorge.toledo@gmail.com>
2018-02-07 21:30:02 -0300

    Teste commit codeship

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
   end
 
+  # Exchange
   def exchange
     value = Currency::exchange params[:currency], params[:currency_destination], params[:quantity].to_f
     render json: {"value": value}
